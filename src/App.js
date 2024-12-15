2a7cbf3d011e34053c69cef9b6359ec3f37c2789
Artem Kruhliak <ar.krugliak@gmail.com>
2024-12-15 23:12:29 +0200

    all work

--- src/App.js
+++ src/App.js
@@ -1,11 +1,56 @@
 import React from "react";
-import GoodsGallery from "./components/GoodsGallery";
+import GoodsCard from "./components/GoodsCard";
 
 function App() {
+  const products = [
+    {
+      id: 1,
+      name: "Яблуко",
+      price: 15,
+      image: `${process.env.PUBLIC_URL}/images/apple.png`,
+    },
+    {
+      id: 2,
+      name: "Груша",
+      price: 20,
+      image: `${process.env.PUBLIC_URL}/images/pear.png`,
+    },
+    {
+      id: 3,
+      name: "Виноград",
+      price: 25,
+      image: `${process.env.PUBLIC_URL}/images/grape.png`,
+    },
+    {
+      id: 4,
+      name: "Персик",
+      price: 18,
+      image: `${process.env.PUBLIC_URL}/images/peach.png`,
+    },
+    {
+      id: 5,
+      name: "Банан",
+      price: 22,
+      image: `${process.env.PUBLIC_URL}/images/banana.png`,
+    },
+    {
+      id: 6,
+      name: "Апельсин",
+      price: 24,
+      image: `${process.env.PUBLIC_URL}/images/orange.png`,
+    },
+  ];
+
   return (
-    <div>
-      <h1 style={{ textAlign: "center" }}>Фрукти</h1>
-      <GoodsGallery />
+    <div className="product-gallery">
+      {products.map((product) => (
+        <GoodsCard
+          key={product.id}
+          image={product.image}
+          name={product.name}
+          price={product.price}
+        />
+      ))}
     </div>
   );
 }
--- src/components/GoodsCard.js
+++ src/components/GoodsCard.js
@@ -1,29 +1,13 @@
 import React from "react";
 
-function GoodsCard(props) {
+function GoodsCard({ image, name, price }) {
   return (
-    <div className="goods-card" style={styles.card}>
-      <img src={props.image} alt={props.name} style={styles.image} />
-      <h3>{props.name}</h3>
-      <p>Cost: {props.price} грн</p>
+    <div className="goods-card">
+      <img src={image} alt={name} />
+      <h3>{name}</h3>
+      <p>Ціна: {price} грн</p>
     </div>
   );
 }
-
-const styles = {
-  card: {
-    border: "1px solid #ccc",
-    borderRadius: "8px",
-    padding: "10px",
-    textAlign: "center",
-    margin: "10px",
-    width: "150px",
-  },
-  image: {
-    width: "100px",
-    height: "100px",
-    marginBottom: "10px",
-  },
-};
 
 export default GoodsCard;
